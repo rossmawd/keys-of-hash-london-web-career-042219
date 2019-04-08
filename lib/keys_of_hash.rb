0e12bfbc3e046e89bbe938bc4dfbd89d635274b4
rossmawd <rossmawd@gmail.com>
2019-04-09 00:46:40 +0100

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    match = []
+    
+    self.each {|key,value|
+      if value == arguments.join
+        match.push(key)
+        next                  #skips to next key/value as argument is single element
+      else
+        arguments.each {|x|
+          if value == x
+            match.push(key)
+          end
+        }
+      end
+    }
+    match
   end
 end
